d382b17fd42dbbaf14f443b347d2e3c166a09bff
sajjadhossein <sajjadhosseinrezaei@yahoo.com>
2024-09-18 15:38:41 +0330

    Databse host change frome 127.0.0.01 to db

--- GemeShop/settings.py
+++ GemeShop/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'shop',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
